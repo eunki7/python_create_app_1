9abf56033a3eb087a5c052941a06242f854a1a7f
eunki7 <outsider7224@hanmail.net>
2019-05-02 17:50:25 +0900

    # 2019-05-02 # 웹 크롤링 수업 관련 섹션 7 추가

---
+++ section7/7-1-2.py
@@ -0,0 +1,162 @@
+# Section7-2
+# Selenium 심화 크롤링(2)
+
+# 시간 처리 관련
+import time
+# bs4 임포트
+from bs4 import BeautifulSoup
+# selenium 관련 임포트
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+# 이미지 다운
+import urllib.request as req
+# 이미지 바이트 처리
+from io import BytesIO
+# 엑셀 처리 임포트
+import xlsxwriter
+
+# 크롬 옵션
+chrome_options = Options()
+# Headless 모드 관련
+chrome_options.add_argument("--headless")
+
+# webdriver 설정(Chrome, Firefox 등) - Headless 모드
+browser = webdriver.Chrome('../webdriver/chrome/chromedriver.exe', options=chrome_options)
+
+# webdriver 설정(Chrome, Firefox 등) - 일반 모드
+# browser = webdriver.Chrome('./webdriver/chrome/chromedriver.exe')
+
+# 엑셀 처리 선언
+workbook = xlsxwriter.Workbook("C:/you_crawl_result.xlsx")
+
+# 워크 시트
+worksheet = workbook.add_worksheet()
+
+# 크롬 브라우저 내부 대기
+browser.implicitly_wait(5)
+
+# 브라우저 사이즈
+# minimize_window() : 최소화
+# maximize_window() : 최대화
+browser.set_window_size(1920, 1280)
+
+# 페이지 이동
+browser.get('https://www.youtube.com/watch?v=XaBfX5wa3EQ')
+
+# 5초 대기
+time.sleep(5)
+
+# html 포커스 주기 위한 코드
+# Explicitly wait(명시적 대기)
+WebDriverWait(browser, 5) \
+    .until(
+    EC.presence_of_element_located((By.TAG_NAME, 'html'))).send_keys(Keys.PAGE_DOWN)
+
+# 2초 대기
+time.sleep(2)
+
+# 페이지 내용
+# print('Before Page Contents : {}'.format(browser.page_source))
+
+# 페이지 이동 시 새로운 데이터 수신 완료 위한 대기 시간
+scraoll_pause_time = 4
+
+# 현재 화면 페이지 높이
+# IE: document.body.scrollHeight
+last_height = browser.execute_script("return document.documentElement.scrollHeight")
+
+print()
+
+# 모든 댓글 데이터가 수신 완료 될 때까지 반복
+while True:
+    # 스크롤바 이동
+    browser.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+
+    # 대기
+    time.sleep(scraoll_pause_time)
+
+    # 스크롤바 이동 -> 새로운 데이터 수신 -> 현재 렌더링 된 화면 높이 새로 구한다.
+    new_height = browser.execute_script("return document.documentElement.scrollHeight")
+
+    # 이전 높이, 새로운 높이 비교
+    print('Last Height : {}, Current Height : {}'.format(last_height, new_height))
+
+    # 새로운 데이터 수신이 없을 경우 종료
+    if new_height == last_height:
+        # While 종료
+        break
+
+    # 높이 변경
+    last_height = new_height
+
+# bs4 초기화
+soup = BeautifulSoup(browser.page_source, "html.parser")
+
+# 통계 리스트 선택자
+top_level = soup.select('div#menu-container yt-formatted-string#text')
+# 댓글 리스트 선택자
+comment = soup.select('ytd-comment-renderer#comment')
+
+# HTML 소스 확인
+# print(soup.prettify())
+
+print()
+print()
+
+# 전체 추천 카운트
+print('Total Like Count : {}'.format(top_level[0].text.strip()))
+# 전체 비추 카운트
+print('Total Dislike Count : {}'.format(top_level[1].text.strip()))
+
+# 엑셀 행 수
+ins_cnt = 2
+
+# Dom 반복
+for dom in comment:
+    print()
+
+    # 이미지 URL 정보
+    img_src = dom.select_one('#img').get('src')
+    # 작성자
+    author = dom.select_one('#author-text > span').text.strip()
+    # 댓글 본문
+    content = dom.select_one('#content-text').text.strip()
+    # 좋아요
+    posi_cnt = dom.select_one('#vote-count-middle').text.strip()
+
+    print('Thumbnail Image URLS : {}'.format(img_src if img_src else 'None'))
+    # 작성자
+    print('Author : {}'.format(author))
+    # 댓글 본문
+    print('Content Text : {}'.format(content))
+    # 좋아요
+    print('Vote Positive Count : {}'.format(posi_cnt))
+
+    # 엑셀 저장(텍스트)
+    worksheet.write('A%s' % ins_cnt, author)
+    worksheet.write('B%s' % ins_cnt, content)
+    worksheet.write('C%s' % ins_cnt, posi_cnt)
+
+    # 엑셀 저장(썸네일 이미지)
+    # 이미지 URL이 없을 경우 처리
+    if img_src:
+        # 이미지 요청 후 바이트 변환
+        img_data = BytesIO(req.urlopen(img_src).read())
+        worksheet.insert_image('D%s' % ins_cnt, author, {'image_data': img_data})
+    else:
+        worksheet.write('D%s' % ins_cnt, 'None')
+
+    # 다음 행 증가
+    ins_cnt += 1
+
+    print()
+
+# 브라우저 종료
+browser.quit()
+
+# 엑셀 파일 닫기
+workbook.close()
